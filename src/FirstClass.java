35c8e78071fed94b4fc8cd4de427077541b53620
Jason D'Oyley <jdfdoyley@gmail.com>
2022-12-10 06:03:41 -0500

    Learn how to write if-statements in Java

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,6 +1,11 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
         System.out.println("Hello, Jason!");
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+        }
     }
 }
