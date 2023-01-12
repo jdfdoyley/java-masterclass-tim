f65e3099c3f2ed1a03aab2a402d94024cc7686a4
Jason D'Oyley <jdfdoyley@gmail.com>
2023-01-12 06:21:26 -0500

    Learn about the for loop and break

--- src/Section_6/TheForLoop.java
+++ src/Section_6/TheForLoop.java
@@ -8,5 +8,35 @@ package Section_6;
  */
 
 public class TheForLoop {
+    public static void main(String[] args) {
 
+        for (int counter = 1; counter <= 5; counter++) {
+            System.out.println(counter);
+        }
+        System.out.println();   // New line
+
+        for (double rate = 2.0; rate <= 5.0; rate++) {
+            double interestAmt = calculateInterest(10000.0, rate);
+            System.out.println("$10,000 at " + rate + "% interest = $" + interestAmt);
+        }
+        System.out.println();   // New line
+
+        for (double rate = 7.5; rate <= 10.0; rate += 0.25) {
+            double interestAmt = calculateInterest(100.0, rate);
+            System.out.println("$100 at " + rate + "% interest = $" + interestAmt);
+        }
+        System.out.println();   // New line
+
+        for (double rate = 7.5; rate <= 10.0; rate += 0.25) {
+            double interestAmt = calculateInterest(100.0, rate);
+
+            if (rate == 8.5) break;
+
+            System.out.println("$100 at " + rate + "% interest = $" + interestAmt);
+        }
+    }
+
+    public static double calculateInterest(double amount, double interestRate) {
+        return (amount * (interestRate / 100));
+    }
 }
