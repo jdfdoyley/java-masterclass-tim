c1aeb7aab7c46170326681bbe0444b3d90739672
Jason D'Oyley <jdfdoyley@gmail.com>
2023-01-12 05:39:40 -0500

    Modify code to use enhance switch statements

--- src/Section_6/Switch.java
+++ src/Section_6/Switch.java
@@ -11,19 +11,13 @@ public class Switch {
         int switchValue = 6;
 
         switch (switchValue) {
-            case 1:
-                System.out.println("Value was 1");
-                break;
-            case 2:
-                System.out.println("Value was 2");
-                break;
-            case 3: case 4: case 5:
+            case 1 -> System.out.println("Value was 1");
+            case 2 -> System.out.println("Value was 2");
+            case 3, 4, 5 -> {
                 System.out.println("Was a 3, a 4, or a 5");
                 System.out.println("Actually it was a " + switchValue);
-                break;
-            default:
-                System.out.println("Was not 1, 2, 3, 4 or 5");
-                break;
+            }
+            default -> System.out.println("Was not 1, 2, 3, 4 or 5");
         }
     }
 }
